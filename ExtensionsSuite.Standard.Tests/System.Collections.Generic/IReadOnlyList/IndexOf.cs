87e71c7e72344b08f0a59d6733dadf2354d1da11
Marc Armbruster <marcarmbruster@hotmail.de>
2018-10-26 08:57:45 +0200

    IReadOnlyList.IndexOf extension added

---
+++ ExtensionsSuite.Standard.Tests/System.Collections.Generic/IReadOnlyList/IndexOf.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExtensionsSuite.Standard.Tests.System.Collections.Generic.IReadOnlyList
+{   
+    [TestClass]
+    public class IndexOf
+    {
+        private class FakeClass
+        {
+            internal string Name { get; set; }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void IndexOfSourceNullTest()
+        {            
+            IReadOnlyList<string> list = null;
+            list.IndexOf<string>("B");
+        }
+
+        [TestMethod]        
+        public void IndexOfFoundTest()
+        {
+            List<string> data = new List<string> { "A", "B", "C", "D" };
+            IReadOnlyList<string> list = data.AsReadOnly();
+            var indexA = list.IndexOf<string>("A");
+            var indexB = list.IndexOf<string>("B");
+            var indexC = list.IndexOf<string>("C");
+            var indexD = list.IndexOf<string>("D");
+            Assert.AreEqual(0, indexA);
+            Assert.AreEqual(1, indexB);
+            Assert.AreEqual(2, indexC);
+            Assert.AreEqual(3, indexD);
+        }
+
+        [TestMethod]
+        public void IndexOfNotFoundTest()
+        {
+            List<string> data = new List<string> { "A", "B", "C", "D" };
+            IReadOnlyList<string> list = data.AsReadOnly();
+            var index = list.IndexOf<string>("X");
+            Assert.AreEqual(-1, index);
+        }
+
+        [TestMethod]
+        public void IndexOfObjectTest()
+        {
+            var fakeOne = new FakeClass { Name = "Marc" };
+            var fakeTwo = new FakeClass { Name = "Gerhard" };
+            List<FakeClass> data = new List<FakeClass> { fakeOne, fakeTwo };
+            IReadOnlyList<FakeClass> list = data.AsReadOnly();
+            var index = list.IndexOf<FakeClass>(fakeTwo);
+            Assert.AreEqual(1, index);
+        }
+    }
+}
---
+++ ExtensionsSuite.Standard/System.Collections.Generic/IReadOnlyListExtensions.cs
@@ -0,0 +1,29 @@
+﻿using ExtensionsSuite.Standard.Suite;
+
+namespace System.Collections.Generic
+{
+    public static class IReadOnlyListExtensions
+    {
+        /// <summary>
+        /// Finds the index of a given element in a collection.
+        /// </summary>
+        /// <typeparam name="T">The item type.</typeparam>
+        /// <param name="source">The source collection.</param>
+        /// <param name="element">The element to search for.</param>
+        /// <returns>The index of the given element. If the collection contains the element, -1 is returned instead.</returns>
+        public static int IndexOf<T>(this IReadOnlyList<T> source, T element)
+        {
+            ValueChecker.ThrowIfNull(source);
+
+            for (int i = 0; i < source.Count; i++)
+            {
+                if (EqualityComparer<T>.Default.Equals(element, source[i]))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
