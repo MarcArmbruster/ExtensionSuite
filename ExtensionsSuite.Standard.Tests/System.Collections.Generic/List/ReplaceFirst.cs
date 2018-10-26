dcb201885a56e813872fc473ef0cbbaea26781c5
Marc Armbruster <marcarmbruster@hotmail.de>
2018-10-26 17:26:32 +0200

    Some collections extensions added

---
+++ ExtensionsSuite.Standard.Tests/System.Collections.Generic/List/ReplaceFirst.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExtensionsSuite.Standard.Tests.System.Collections.Generic.List
+{
+    [TestClass]
+    public class ReplaceFirst
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ReplaceFirstSourceNullTest()
+        {
+            List<string> data = null;
+            data.ReplaceFirst(s => s == "Y", "A");
+        }
+
+        [TestMethod]
+        public void ReplaceFirstPositiveTest()
+        {
+            List<string> data = new List<string> { "X", "Y", "Z" };
+            data.ReplaceFirst(s => s == "Y", "A");
+
+            CollectionAssert.AllItemsAreNotNull(data);
+            CollectionAssert.AllItemsAreUnique(data);
+            CollectionAssert.Contains(data, "X");
+            CollectionAssert.Contains(data, "A");
+            CollectionAssert.Contains(data, "Z");
+            Assert.AreEqual(1, data.IndexOf("A"));
+        }
+
+        [TestMethod]
+        public void ReplaceFirstNotFoundTest()
+        {
+            List<string> data = new List<string> { "X", "Y", "Z" };
+            data.ReplaceFirst(s => s == "W", "A");
+
+            CollectionAssert.AllItemsAreNotNull(data);
+            CollectionAssert.AllItemsAreUnique(data);
+            CollectionAssert.Contains(data, "X");
+            CollectionAssert.Contains(data, "Y");
+            CollectionAssert.Contains(data, "Z");
+            Assert.AreEqual(-1, data.IndexOf("A"));
+        }
+
+        [TestMethod]
+        public void ReplaceFirstNullValueTest()
+        {
+            List<string> data = new List<string> { "X", "Y", "Z" };
+            data.ReplaceFirst(s => s == "X", null);
+
+            CollectionAssert.AllItemsAreUnique(data);
+            CollectionAssert.Contains(data, null);
+            CollectionAssert.Contains(data, "Y");
+            CollectionAssert.Contains(data, "Z");
+            Assert.IsNull(data[0]);
+        }
+    }
+}
---
+++ ExtensionsSuite.Standard/System.Collections.Generic/ICollectionExtensions.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+using ExtensionsSuite.Standard.Suite;
+
+namespace System.Collections.Generic
+{
+    public static class CollectionExtension
+    {
+        /// <summary>
+        /// Adds a range of items to the source collection.
+        /// </summary>
+        /// <typeparam name="T">The item type.</typeparam>
+        /// <param name="source">The source collection.</param>
+        /// <param name="items">The items to add to the source collection.</param>
+        public static void AddRange<T>(this ICollection<T> source, IEnumerable<T> items)
+        {
+            ValueChecker.ThrowIfNull(source);
+
+            if (source is List<T> list)
+            {
+                list.AddRange(items);
+            }
+            else
+            {
+                foreach (T item in items)
+                {
+                    source.Add(item);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Removes a range of items from the source collection.
+        /// </summary>
+        /// <typeparam name="T">The item type.</typeparam>
+        /// <param name="source">The source collection.</param>
+        /// <param name="items">The items to remove from the source collection.</param>
+        public static void RemoveRange<T>(this ICollection<T> source, IEnumerable<T> items)
+        {
+            ValueChecker.ThrowIfNull(source);
+
+            foreach (T item in items.ToList())
+            {
+                source.Remove(item);
+            }
+        }
+    }
+}
