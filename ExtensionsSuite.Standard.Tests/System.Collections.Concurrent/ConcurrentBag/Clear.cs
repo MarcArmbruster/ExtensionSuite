18d46f865c50084086d2f1eb20216826818a49cc
Marc Armbruster <marcarmbruster@hotmail.de>
2018-10-26 17:34:12 +0200

    ConcurrentBag.Clear() added

---
+++ ExtensionsSuite.Standard.Tests/System.Collections.Concurrent/ConcurrentBag/Clear.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Concurrent;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExtensionsSuite.Standard.Tests.System.Collections.Concurrent.ConcurrentBag
+{
+    [TestClass]
+    public class Clear
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ReplaceFirstSourceNullTest()
+        {
+            ConcurrentBag<string> data = null;
+            data.Clear();
+        }
+
+        [TestMethod]
+        public void ClearBag()
+        {
+            ConcurrentBag<string> data = new ConcurrentBag<string> { "X", "Y", "Z" };
+            data.Clear();
+            Assert.AreEqual(0, data.Count);
+        }
+
+        [TestMethod]
+        public void ClearEmptyBag()
+        {
+            ConcurrentBag<string> data = new ConcurrentBag<string>();
+            data.Clear();
+            Assert.AreEqual(0, data.Count);
+        }
+    }
+}
---
+++ ExtensionsSuite.Standard/System.Collections.Concurrent/ConcurrentBagExtension.cs
@@ -0,0 +1,22 @@
+﻿using ExtensionsSuite.Standard.Suite;
+
+namespace System.Collections.Concurrent
+{
+    public static class ConcurrentBagExtension
+    {
+        /// <summary>
+        /// Clears the specified source.
+        /// </summary>
+        /// <typeparam name="T">The type of the elements.</typeparam>
+        /// <param name="source">The source concurrent bag.</param>
+        public static void Clear<T>(this ConcurrentBag<T> source)
+        {
+            ValueChecker.ThrowIfNull(source);
+
+            while (!source.IsEmpty)
+            {
+                source.TryTake(out _);
+            }
+        }
+    }
+}
