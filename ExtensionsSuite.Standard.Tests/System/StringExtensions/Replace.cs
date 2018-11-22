c9981be4fa818388e106ce5ce4106ed3f60e8585
Gerhard Ahrens <ahrensg1@rochedc.com>
2018-11-22 08:36:15 +0100

    Test für Replace

--- ExtensionsSuite.Standard.Tests/System/StringExtensions/Replace.cs
+++ ExtensionsSuite.Standard.Tests/System/StringExtensions/Replace.cs
@@ -13,5 +13,21 @@ namespace ExtensionsSuite.Standard.Tests.System.StringExtensions
             string source = null;
             source.Replace("AA","??",StringComparison.CurrentCultureIgnoreCase);
         }
+
+        [TestMethod]
+        public void ReplaceStringLowerTest()
+        {
+            string source = "String Text original!";
+            string replaceText = source.Replace("original","replaced",StringComparison.CurrentCultureIgnoreCase);
+            Assert.AreEqual("String Text replaced!", replaceText);
+        }
+
+        [TestMethod]
+        public void ReplaceStringUpperLowerTest()
+        {
+            string source = "String Text ORIGINAL!";
+            string replaceText = source.Replace("original", "REPLACED", StringComparison.CurrentCultureIgnoreCase);
+            Assert.AreEqual("String Text REPLACED!", replaceText);
+        }
     }
 }
