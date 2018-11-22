b3aa370e38f7d19ca78f99968f9afeb13996b49f
Gerhard Ahrens <ahrensg1@rochedc.com>
2018-11-22 08:29:41 +0100

    Test für Replace Extension

---
+++ ExtensionsSuite.Standard.Tests/System/StringExtensions/Replace.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExtensionsSuite.Standard.Tests.System.StringExtensions
+{
+    [TestClass]
+    public class Replace
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ReplaceWithNull()
+        {
+            string source = null;
+            source.Replace("AA","??",StringComparison.CurrentCultureIgnoreCase);
+        }
+    }
+}
