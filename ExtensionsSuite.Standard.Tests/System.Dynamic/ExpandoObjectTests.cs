6f1b80f63bcb3e2ea75a95bb4b21ad4df5fdb46b
Marc Armbruster <marcarmbruster@hotmail.de>
2024-02-01 18:49:50 +0100

    ExpandoObject

---
+++ ExtensionsSuite.Standard.Tests/System.Dynamic/ExpandoObjectTests.cs
@@ -0,0 +1,113 @@
+﻿namespace ExtensionsSuite.Standard.Tests.System.Dynamic
+{
+    using global::System;
+    using global::System.Data;
+    using global::System.Dynamic;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    /// <summary>
+    /// Summary description for ExpandoObjectTests
+    /// </summary>
+    [TestClass]
+    public class ExpandoObjectTests
+    {
+        [TestMethod]
+        public void ContainsPropertyTest()
+        {
+            var target = new ExpandoObject();
+
+            target.SetPropertyValue<string>("Text", "Hello");
+            target.SetPropertyValue<int>("Number", 12);
+
+            Assert.IsTrue(target.ContainsProperty("Text"));
+            Assert.IsFalse(target.ContainsProperty("text"));
+
+            Assert.IsTrue(target.ContainsProperty("Number"));
+            Assert.IsFalse(target.ContainsProperty("NumBer"));
+
+            Assert.IsFalse(target.ContainsProperty("AnyOther"));
+        }
+
+        [TestMethod]
+        public void GetPropertyValueTest()
+        {
+            var target = new ExpandoObject();
+
+            target.SetPropertyValue<string>("Text", "Hello");
+            target.SetPropertyValue<int>("Number", 12);
+
+            Assert.AreEqual("Hello", target.GetPropertyValue<string>("Text"));
+            Assert.IsNull(target.GetPropertyValue<object>("Something"));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidCastException))]
+        public void GetPropertyValueInvalidCastTest()
+        {
+            var target = new ExpandoObject();
+
+            target.SetPropertyValue<string>("Text", "Hello");
+
+            Assert.AreEqual(default(decimal), target.GetPropertyValue<decimal>("Text"));
+        }
+
+        [TestMethod]
+        public void TryGetPropertyValueFoundTest()
+        {
+            var target = new ExpandoObject();
+
+            target.SetPropertyValue<string>("Text", "Hello");
+
+            string value;
+            bool success = target.TryGetPropertyValue<string>("Text", out value);
+            Assert.IsTrue(success);
+            Assert.AreEqual("Hello", value);
+        }
+
+        [TestMethod]
+        public void TryGetPropertyValueNotFoundTest()
+        {
+            var target = new ExpandoObject();
+
+            target.SetPropertyValue<string>("Text2", "Hello");
+
+            string value;
+            bool success = target.TryGetPropertyValue<string>("Text", out value);
+            Assert.IsFalse(success);
+            Assert.AreEqual(default(string), value);
+        }
+
+        [TestMethod]
+        public void CreatePropertyTest()
+        {
+            var target = new ExpandoObject();
+
+            target.CreateProperty<string>("ColorCode");
+            
+            Assert.IsTrue(target.ContainsProperty("ColorCode"));
+            Assert.IsTrue(target.IsNullOrDefault<string>("ColorCode"));
+        }
+
+        [TestMethod]
+        public void IsNullTest()
+        {
+            var target = new ExpandoObject();
+
+            target.CreateProperty<DataTable>("ColorTabke");
+
+            Assert.IsTrue(target.IsNull("ColorTabke"));
+        }
+
+        [TestMethod]
+        public void SetPropertyTest()
+        {
+            var target = new ExpandoObject();
+
+            target.SetPropertyValue<string>("ColorCode", "Blue");
+            target.SetPropertyValue<string>("ColorCode", "Green");
+
+            Assert.IsTrue(target.ContainsProperty("ColorCode"));
+            Assert.AreEqual("Green", target.GetPropertyValue<string>("ColorCode"));
+        }
+    }
+}
---
+++ ExtensionsSuite.Standard/System.Dynamic/ExpandoObjectExtensions.cs
@@ -0,0 +1,112 @@
+﻿namespace System.Dynamic
+{
+    using System.Collections.Generic;
+
+    public static class ExpandoObjectExtensions
+    {
+        /// <summary>
+        /// Verifies whether the target instances contains a property with the given name.
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="propertyName"></param>
+        /// <returns>True if property exists; false otherwise.</returns>
+        public static bool ContainsProperty(this ExpandoObject target, string propertyName)
+        {
+            return ((IDictionary<string, object>)target).ContainsKey(propertyName);
+        }
+
+        /// <summary>
+        /// Gets the property value. 
+        /// </summary>
+        /// <typeparam name="T">Type of the property.</typeparam>
+        /// <param name="target">Target instance.</param>
+        /// <param name="propertyName">Name of the property.</param>
+        /// <returns>The property value. Default type value if not found.</returns>
+        public static T GetPropertyValue<T>(this ExpandoObject target, string propertyName)
+        {
+            if (((IDictionary<string, object>)target).TryGetValue(propertyName, out var value))
+            {
+                return (T)value;
+            }
+
+            return default;
+        }
+
+        /// <summary>
+        /// Tries t o get the value for the named property.
+        /// </summary>
+        /// <typeparam name="T">Type of the property.</typeparam>
+        /// <param name="target">Target instance.</param>
+        /// <param name="propertyName">Name of the property.</param>
+        /// <param name="value">Out: The value.</param>
+        /// <returns>True if a property is found</returns>
+        public static bool TryGetPropertyValue<T>(this ExpandoObject target, string propertyName, out T value)
+        {
+            if (((IDictionary<string, object>)target).TryGetValue(propertyName, out var innerValue))
+            {
+                value = (T)innerValue;
+                return true;
+            }
+
+            value = default;
+            return false;
+        }
+
+        /// <summary>
+        /// Creates (or overrides) the dynamic property. 
+        /// </summary>
+        /// <typeparam name="T">Type of the property.</typeparam>
+        /// <param name="target">Target instance.</param>
+        /// <param name="propertyName">Name of the property.</param>
+        public static void CreateProperty<T>(this ExpandoObject target, string propertyName)
+        {
+            ((IDictionary<string, object>)target)[propertyName] = default(T);
+        }
+
+        /// <summary>
+        /// Sets the value to the dynamic property. If the property does not exists, it will be created.
+        /// </summary>
+        /// <typeparam name="T">Type of the property.</typeparam>
+        /// <param name="target">Target instance.</param>
+        /// <param name="propertyName">Name of the property.</param>
+        /// <param name="value">The value of the property</param>
+        public static void SetPropertyValue<T>(this ExpandoObject target, string propertyName, T value)
+        {
+            ((IDictionary<string, object>)target)[propertyName] = value;
+        }
+
+        /// <summary>
+        /// Checks whether the property value is null.
+        /// </summary>
+        /// <param name="target">Target instance.</param>
+        /// <param name="propertyName">Name of the property.</param>
+        /// <returns>True if null, false otherwise.</returns>
+        public static bool IsNull(this ExpandoObject target, string propertyName)
+        {
+            if (target.TryGetPropertyValue<object>(propertyName, out var value))
+            {
+                return value == null;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Checks whether the property value is null or equals the default value for the given type.
+        /// </summary>
+        /// <typeparam name="T">Type of the property.</typeparam>
+        /// <param name="target">Target instance.</param>
+        /// <param name="propertyName">Name of the property.</param>
+        /// <returns>True if null, false otherwise.</returns>
+
+        public static bool IsNullOrDefault<T>(this ExpandoObject target, string propertyName)
+        {
+            if (target.TryGetPropertyValue<T>(propertyName, out T value))
+            {
+                return value == null || default(T).Equals(value);
+            }
+
+            return true;
+        }
+    }
+}
